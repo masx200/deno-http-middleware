aa34d6f9ce1f79a64194ca5f27f54e111caba7d8
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-11 20:39:14 +0800

    Update etag-conditional_get.test.ts

--- test/etag-conditional_get.test.ts
+++ test/etag-conditional_get.test.ts
@@ -6,7 +6,7 @@ import { etag_builder } from "../middleware/etag_builder.ts";
 import { logger } from "../middleware/logger.ts";
 import { createHandler } from "../src/createHandler.ts";
 
-Deno.test("etag-conditional_get-non-empty-body", async () => {
+Deno.test("etag-conditional_get-non-empty-body-text", async () => {
     const controller = new AbortController();
     const port = Math.floor(Math.random() * 20000 + 30000);
     const handler = createHandler([
@@ -87,7 +87,70 @@ Deno.test("etag-conditional_get-empty-body", async () => {
             const text = await response.text();
 
             assertEquals(text, "");
-            assert(response.headers.get("etag"));
+            const etag = response.headers.get("etag");
+            assert(etag);
+            {
+                const url = `http://localhost:${port}/test`;
+                const response = await fetch(url, {
+                    headers: { "if-none-match": etag },
+                });
+                console.log(response);
+                assert(response.ok === false);
+                assertEquals(response.status, 304);
+                const text = await response.text();
+
+                assertEquals(text, "");
+            }
+        }
+    } finally {
+        controller.abort();
+    }
+    await p;
+});
+Deno.test("etag-conditional_get-non-empty-body-json", async () => {
+    const controller = new AbortController();
+    const port = Math.floor(Math.random() * 20000 + 30000);
+    const handler = createHandler([
+        logger,
+        conditional_get,
+        etag_builder,
+        () => {
+            return {
+                body: JSON.stringify(["qwerty", { json: true }]),
+                headers: { "content-type": "application/json" },
+            };
+        },
+    ]);
+
+    const { signal } = controller;
+    const p = serve(handler, { signal, port: port });
+    try {
+        const etag = await (async () => {
+            const url = `http://localhost:${port}/test`;
+            const response = await fetch(url);
+            console.log(response);
+            assert(response.ok);
+            assertEquals(response.status, 200);
+            const json = await response.json();
+            const headers = response.headers;
+            assert(headers.get("Content-Type")?.startsWith("application/json"));
+            assertEquals(json, ["qwerty", { json: true }]);
+            return response.headers.get("etag");
+        })();
+        assert(etag);
+        {
+            const url = `http://localhost:${port}/test`;
+            const response = await fetch(url, {
+                headers: { "if-none-match": etag },
+            });
+            const headers = response.headers;
+            assert(headers.get("Content-Type")?.startsWith("application/json"));
+            console.log(response);
+            assert(response.ok === false);
+            assertEquals(response.status, 304);
+            const text = await response.text();
+
+            assertEquals(text, "");
         }
     } finally {
         controller.abort();
