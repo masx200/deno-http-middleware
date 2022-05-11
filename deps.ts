a8be4116eefada87a8f59fe3cedcd3afef811d3c
masx200 <masx200@qq.com>
2022-05-11 04:07:11 +0000

    init

---
+++ deps.ts
@@ -0,0 +1,16 @@
+import {
+    assert,
+    assertInstanceOf,
+} from "https://deno.land/std@0.138.0/testing/asserts.ts";
+
+export { assert, assertInstanceOf };
+export type {
+    ConnInfo,
+    Handler,
+} from "https://deno.land/std@0.138.0/http/server.ts";
+export { STATUS_TEXT } from "https://deno.land/std@0.138.0/http/http_status.ts";
+import { encode } from "https://deno.land/std@0.138.0/encoding/hex.ts";
+export { encode };
+export { default as isPlainObject } from "https://cdn.skypack.dev/lodash@4.17.21/isPlainObject?dts";
+
+export { default as fresh } from "https://cdn.skypack.dev/fresh@0.5.2?dts";
