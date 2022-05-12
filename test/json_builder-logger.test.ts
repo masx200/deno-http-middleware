8d68a1610a9632abe5b593d916166fed99d74a44
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-12 10:13:37 +0800

    Update json_builder-logger.test.ts

--- test/json_builder-logger.test.ts
+++ test/json_builder-logger.test.ts
@@ -13,9 +13,10 @@ Deno.test("json_builder-logger-object", async () => {
         json_builder,
         (ctx) => {
             const { pathname } = new URL(ctx.request.url);
-            const body = pathname === "/array"
-                ? ["hello world,", ctx.request.url]
-                : { a: 1, b: 2, c: ctx.request.url };
+            const body =
+                pathname === "/array"
+                    ? ["hello world,", ctx.request.url]
+                    : { a: 1, b: 2, c: ctx.request.url };
             return { body };
         },
     ]);
@@ -31,7 +32,7 @@ Deno.test("json_builder-logger-object", async () => {
         assert(headers.get("Content-Type")?.startsWith("application/json"));
         assertEquals(response.status, 200);
         const json = await response.json();
-
+        console.log(json);
         assertEquals(json, { a: 1, b: 2, c: url });
     } finally {
         controller.abort();
@@ -47,9 +48,10 @@ Deno.test("json_builder-logger-array", async () => {
         json_builder,
         (ctx) => {
             const { pathname } = new URL(ctx.request.url);
-            const body = pathname === "/array"
-                ? ["hello world,", ctx.request.url]
-                : { a: 1, b: 2, c: ctx.request.url };
+            const body =
+                pathname === "/array"
+                    ? ["hello world,", ctx.request.url]
+                    : { a: 1, b: 2, c: ctx.request.url };
             return { body };
         },
     ]);
@@ -65,8 +67,59 @@ Deno.test("json_builder-logger-array", async () => {
         assert(headers.get("Content-Type")?.startsWith("application/json"));
         assertEquals(response.status, 200);
         const json = await response.json();
-
+        console.log(json);
         assertEquals(json, ["hello world,", url]);
+    } finally {
+        controller.abort();
+    }
+    await p;
+});
+
+Deno.test("json_builder-logger-request", async () => {
+    const controller = new AbortController();
+    const port = Math.floor(Math.random() * 30000 + 20000);
+    const handler = createHandler([
+        logger,
+        json_builder,
+        async (ctx) => {
+            const { request } = ctx;
+            const { method } = request;
+            const { pathname } = new URL(ctx.request.url);
+            const text = await request.text();
+            const body = {
+                method,
+                text,
+                pathname,
+                "x-random-data": ctx.request.headers.get("x-random-data"),
+            };
+            return { body };
+        },
+    ]);
+
+    const { signal } = controller;
+    const p = serve(handler, { signal, port: port });
+    try {
+        const url = `http://localhost:${port}/request`;
+        const response = await fetch(url, {
+            body: "1234567890",
+            method: "POST",
+            headers: {
+                "x-random-data": "9876543210",
+            },
+        });
+        const headers = response.headers;
+        console.log(response);
+        assert(response.ok);
+        assert(headers.get("Content-Type")?.startsWith("application/json"));
+        assertEquals(response.status, 200);
+        const json = await response.json();
+        console.log(json);
+        assertEquals(json, {
+            pathname: "/request",
+            text: "1234567890",
+            method: "POST",
+            "x-random-data": "9876543210",
+        });
     } finally {
         controller.abort();
     }
