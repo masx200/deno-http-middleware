71f4a7c0c3cb06d2f16959d6cfecaa74095c706d
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-12 11:31:01 +0800

    Update stream-etag.test.ts

--- test/stream-etag.test.ts
+++ test/stream-etag.test.ts
@@ -4,6 +4,47 @@ import { conditional_get } from "../middleware/conditional_get.ts";
 import { logger } from "../middleware/logger.ts";
 import { stream_etag } from "../middleware/stream_etag.ts";
 import { createHandler } from "../src/createHandler.ts";
+Deno.test(
+    "etag-conditional_get-stream-body-greater-than-sizelimit",
+    async () => {
+        const filename = new URL("../README.md", import.meta.url);
+        const controller = new AbortController();
+        const port = Math.floor(Math.random() * 20000 + 30000);
+        const handler = createHandler([
+            logger,
+            conditional_get,
+            stream_etag({ sizelimit: 1000 }),
+            async () => {
+                const file = await Deno.open(filename, { read: true });
+                console.log(file);
+                const body = file.readable;
+                return { body: body };
+            },
+        ]);
+
+        const { signal } = controller;
+        const p = serve(handler, { signal, port: port });
+        try {
+            {
+                const url = `http://localhost:${port}/test`;
+                const response = await fetch(url);
+                console.log(response);
+                const text = await response.text();
+                console.log(text);
+                assert(response.ok);
+                assertEquals(response.status, 200);
+
+                const content = await Deno.readTextFile(filename);
+                assertEquals(text, content);
+                const etag = response.headers.get("etag");
+                assert(typeof etag !== "string");
+            }
+        } finally {
+            controller.abort();
+        }
+        await p;
+    }
+);
 
 Deno.test(
     "etag-conditional_get-stream-body-smaller-than-sizelimit",
