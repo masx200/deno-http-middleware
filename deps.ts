d420db0e173a776476aa0201804c930bfc41cd86
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-05 10:31:01 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -3,20 +3,20 @@ export {
     assertEquals,
     assertInstanceOf,
     assertNotEquals,
-} from "https://deno.land/std@0.140.0/testing/asserts.ts";
+} from "https://deno.land/std@0.142.0/testing/asserts.ts";
 
 export type {
     ConnInfo,
     Handler,
-} from "https://deno.land/std@0.140.0/http/server.ts";
-export { STATUS_TEXT } from "https://deno.land/std@0.140.0/http/http_status.ts";
-import { encode } from "https://deno.land/std@0.140.0/encoding/hex.ts";
-import { assertFalse } from "https://deno.land/std@0.140.0/testing/asserts.ts";
+} from "https://deno.land/std@0.142.0/http/server.ts";
+export { STATUS_TEXT } from "https://deno.land/std@0.142.0/http/http_status.ts";
+import { encode } from "https://deno.land/std@0.142.0/encoding/hex.ts";
+import { assertFalse } from "https://deno.land/std@0.142.0/testing/asserts.ts";
 export { encode };
 export { default as isPlainObject } from "https://cdn.skypack.dev/lodash@4.17.21/isPlainObject?dts";
 
 export { default as fresh } from "https://cdn.skypack.dev/fresh@0.5.2?dts";
-export { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.142.0/http/server.ts";
 export { assertFalse };
-import { METHODS } from "https://deno.land/std@0.140.0/node/http.ts";
+import { METHODS } from "https://deno.land/std@0.142.0/node/http.ts";
 export { METHODS };
