1ba0649807c592abf52289bca5173d57a6f99b73
masx200 <34191203+masx200@users.noreply.github.com>
2022-06-06 09:45:52 +0800

    Update concatArrayBuffer.ts

--- utils/concatArrayBuffer.ts
+++ utils/concatArrayBuffer.ts
@@ -1,7 +1,5 @@
-import { bodyToBuffer } from "../body/bodyToBuffer.ts";
-
 export async function concatArrayBuffer(
     array: Uint8Array[],
 ): Promise<Uint8Array> {
-    return await bodyToBuffer(new Blob(array));
+    return new Uint8Array(await new Blob(array).arrayBuffer());
 }
