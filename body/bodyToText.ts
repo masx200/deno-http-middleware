dac833edd41a2cdf6f11406a07cbb6692df79232
masx200 <34191203+masx200@users.noreply.github.com>
2022-07-01 23:05:58 +0800

    Update bodyToText.ts

--- body/bodyToText.ts
+++ body/bodyToText.ts
@@ -1,3 +1,6 @@
-export async function bodyToText(body?: BodyInit | null): Promise<string> {
-    return await new Response(body).text();
+export async function bodyToText(
+body?: BodyInit | null,
+headers?: HeadersInit,
+): Promise<string> {
+    return await new Response(body,{headers}).text();
 }
