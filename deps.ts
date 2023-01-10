9dcd8aa0ebe0a90d1a8a9f1981ec062a047dacbc
GitHub <noreply@github.com>
2023-01-10 19:42:43 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -15,10 +15,10 @@ import {
 export type { ConnInfo };
 export { STATUS_TEXT } from "https://deno.land/std@0.171.0/http/http_status.ts";
 export { encode };
-export { default as isPlainObject } from "https://cdn.skypack.dev/lodash@4.17.21/isPlainObject?dts";
+export { default as isPlainObject } from "https://cdn.skypack.dev/lodash@undefined/isPlainObject?dts";
 
 export { default as fresh } from "https://cdn.skypack.dev/fresh@0.5.2?dts";
 export { serve };
 export { assertFalse };
 export { METHODS };
-export { html, json, text } from "https://esm.sh/@hattip/response@0.0.24";
+export { html, json, text } from "https://esm.sh/@hattip/response@undefined";
