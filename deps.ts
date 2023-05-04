e41b3f40e00c5c1aa1e510223433907e8e09afca
GitHub <noreply@github.com>
2023-05-04 19:42:36 +0000

    Update dependencies

--- deps.ts
+++ deps.ts
@@ -3,17 +3,17 @@ export {
     assertEquals,
     assertInstanceOf,
     assertNotEquals,
-} from "https://deno.land/std@0.185.0/testing/asserts.ts";
-import { encode } from "https://deno.land/std@0.185.0/encoding/hex.ts";
+} from "https://deno.land/std@0.186.0/testing/asserts.ts";
+import { encode } from "https://deno.land/std@0.186.0/encoding/hex.ts";
 import { METHODS } from "node:http";
-import { assertFalse } from "https://deno.land/std@0.185.0/testing/asserts.ts";
+import { assertFalse } from "https://deno.land/std@0.186.0/testing/asserts.ts";
 import {
     ConnInfo,
     serve_http as serve,
 } from "https://deno.land/x/masx200_deno_serve_https@1.0.6/mod.ts";
 
 export type { ConnInfo };
-export { STATUS_TEXT } from "https://deno.land/std@0.185.0/http/http_status.ts";
+export { STATUS_TEXT } from "https://deno.land/std@0.186.0/http/http_status.ts";
 export { encode };
 export { default as isPlainObject } from "https://cdn.skypack.dev/lodash@4.17.21/isPlainObject?dts";
 
