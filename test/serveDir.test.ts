75142cd838a054931cd69a326c2cc89fb246d6ff
masx200 <34191203+masx200@users.noreply.github.com>
2023-05-31 21:28:07 +0800

    Create serveDir.test.ts

---
+++ test/serveDir.test.ts
@@ -0,0 +1,46 @@
+import {
+    assert,
+    assertEquals,
+} from "https://deno.land/std@0.189.0/testing/asserts.ts";
+
+import { getOriginalOptions } from "../src/getOriginalOptions.ts";
+import { getOriginalRequest } from "../src/getOriginalRequest.ts";
+import { handler } from "../src/createHandler.ts";
+import { serveDir } from "https://deno.land/std@0.190.0/http/file_server.ts";
+import { serve_http } from "https://deno.land/x/masx200_deno_serve_https@1.0.6/mod.ts";
+
+Deno.test("serveDir", async () => {
+    const port = Math.floor(Math.random() * 55535 + 10000);
+    const app = handler(async (ctx, _next) => {
+        console.log({
+            request: getOriginalRequest(ctx),
+            options: getOriginalOptions(ctx),
+        });
+        return serveDir(new Request(ctx.request.url, ctx.request), {
+            fsRoot: ".",
+            urlRoot: "",
+            showDirListing: true,
+            showDotfiles: true,
+            showIndex: true,
+        });
+    });
+
+    const ac = new AbortController();
+    const ps = serve_http(app, {
+        signal: ac.signal,
+        port,
+    });
+    try {
+        const res = await fetch(`http://localhost:${port}/README.md`);
+
+        const text = await res.text();
+        assert(text.length);
+        const file = await Deno.readTextFile("./README.md");
+        assertEquals(text, file);
+    } catch (error) {
+        throw error;
+    } finally {
+        ac.abort();
+        await ps;
+    }
+});
