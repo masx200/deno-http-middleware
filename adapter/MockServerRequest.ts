2fbdca88b26389f53c29885c75c7bfd473a5152e
masx200 <34191203+masx200@users.noreply.github.com>
2023-06-01 14:47:07 +0800

    export class MockServerRequest extends IncomingMessage implements PassThrough {

--- adapter/MockServerRequest.ts
+++ adapter/MockServerRequest.ts
@@ -1,11 +1,17 @@
+import { PassThrough, Readable } from "node:stream";
+
 import { IncomingMessage } from "node:http";
 import { Socket } from "node:net";
+import { TransformCallback } from "node:stream";
 
-export class MockServerRequest extends IncomingMessage {
+//@ts-ignore
+export class MockServerRequest extends IncomingMessage implements PassThrough {
     #request: Request;
 
     constructor(socket: Socket, request: Request) {
         super(socket);
+        //@ts-ignore
+        PassThrough.call(this);
         this.#request = request;
 
         this.method = this.#request.method;
@@ -24,5 +30,80 @@ export class MockServerRequest extends IncomingMessage {
         for (const [key, value] of request.headers) {
             this.headers[key] = value;
         }
+        if (request.body) {
+            //@ts-ignore
+            Readable.fromWeb(request.body).pipe(this);
+        } else {
+            this.end();
+        }
+    }
+    _transform(
+        chunk: any,
+        encoding: BufferEncoding,
+        callback: TransformCallback,
+    ): void {
+        PassThrough.prototype._transform.call(this, chunk, encoding, callback);
+    }
+    _flush(callback: TransformCallback): void {
+        PassThrough.prototype._flush.call(this, callback);
+    }
+    writable: boolean = false;
+    writableEnded: boolean = false;
+    writableFinished: boolean = false;
+    writableHighWaterMark: number = 0;
+    writableLength: number = 0;
+    writableObjectMode: boolean = false;
+    writableCorked: number = 0;
+    writableNeedDrain: boolean = false;
+    allowHalfOpen: boolean = false;
+    _write(
+        chunk: any,
+        encoding: BufferEncoding,
+        callback: (error?: Error | null | undefined) => void,
+    ): void {
+        PassThrough.prototype._write.call(this, chunk, encoding, callback);
+    }
+    _writev?(
+        chunks: { chunk: any; encoding: BufferEncoding }[],
+        callback: (error?: Error | null | undefined) => void,
+    ): void {
+        PassThrough.prototype._writev?.call(this, chunks, callback);
+    }
+    _final(callback: (error?: Error | null | undefined) => void): void {
+        PassThrough.prototype._final.call(this, callback);
+    }
+    write(
+        chunk: any,
+        encoding?: BufferEncoding | undefined,
+        cb?: ((error: Error | null | undefined) => void) | undefined,
+    ): boolean;
+    write(
+        chunk: any,
+        cb?: ((error: Error | null | undefined) => void) | undefined,
+    ): boolean;
+    write(chunk: unknown, encoding?: unknown, cb?: unknown): boolean {
+        //@ts-ignore
+        return PassThrough.prototype.write.call(this, chunk, encoding, cb);
+    }
+    setDefaultEncoding(encoding: BufferEncoding): this {
+        //@ts-ignore
+        return PassThrough.prototype.setDefaultEncoding.call(this, encoding);
+    }
+    end(cb?: (() => void) | undefined): this;
+    end(chunk: any, cb?: (() => void) | undefined): this;
+    end(
+        chunk: any,
+        encoding?: BufferEncoding | undefined,
+        cb?: (() => void) | undefined,
+    ): this;
+    end(chunk?: unknown, encoding?: unknown, cb?: unknown): this {
+        //@ts-ignore
+        return PassThrough.prototype.end.call(this, chunk, encoding, cb);
+    }
+    cork(): void {
+        PassThrough.prototype.cork.call(this);
+    }
+    uncork(): void {
+        PassThrough.prototype.uncork.call(this);
     }
 }
