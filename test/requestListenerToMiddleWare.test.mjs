99a811237ebb29912e9b0e77684699b2a99cbf08
masx200 <34191203+masx200@users.noreply.github.com>
2023-06-01 19:21:54 +0800

    测试

--- test/requestListenerToMiddleWare.test.mjs
+++ test/requestListenerToMiddleWare.test.mjs
@@ -123,3 +123,47 @@ test("requestListenerToMiddleWare", async () => {
         server.close();
     }
 });
+
+test("requestListenerToMiddleWare 404", async () => {
+    const port = Math.floor(Math.random() * 55535 + 10000);
+    const server = createServer(
+        listener(
+            async (_ctx, next) => {
+                try {
+                    await next();
+                } catch (error) {
+                    return new Response(error?.message + "\n" + error?.stack, {
+                        status: 500,
+                    });
+                }
+            },
+            requestListenerToMiddleWare((req, res) => {
+                console.log(req, res);
+                res.statusCode = 404;
+                // debugger;
+                res.end("not found!");
+            }),
+            () => ({ status: 200, body: "hello world!" }),
+        ),
+    );
+    try {
+        await new Promise((res) => {
+            server.listen(port, () => {
+                console.log("http server listening port:" + port);
+
+                res();
+            });
+        });
+        const res = await fetch("http://127.0.0.1:" + port);
+        console.log(res);
+        const data = await res.text();
+        console.log(data);
+        assert.equal(res.status, 200);
+
+        assert.equal(data, "hello world!");
+    } catch (e) {
+        throw e;
+    } finally {
+        server.close();
+    }
+});
