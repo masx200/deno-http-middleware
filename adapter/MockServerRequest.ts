b14a20fd122126015d94432e4d1b7d1d3787c785
masx200 <34191203+masx200@users.noreply.github.com>
2023-06-01 14:10:11 +0800

    Update MockServerRequest.ts

--- adapter/MockServerRequest.ts
+++ adapter/MockServerRequest.ts
@@ -3,6 +3,7 @@ import { Socket } from "node:net";
 
 export class MockServerRequest extends IncomingMessage {
     #request: Request;
+
     constructor(socket: Socket, request: Request) {
         super(socket);
         this.#request = request;
