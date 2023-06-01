84f29b8dd936cd6d32d3f7f12d6a46773b740827
masx200 <34191203+masx200@users.noreply.github.com>
2023-06-01 13:13:32 +0800

    Update MockServerRequest.ts

--- adapter/MockServerRequest.ts
+++ adapter/MockServerRequest.ts
@@ -6,5 +6,22 @@ export class MockServerRequest extends IncomingMessage {
     constructor(socket: Socket, request: Request) {
         super(socket);
         this.#request = request;
+
+        this.method = this.#request.method;
+
+        const urlobj = new URL(request.url);
+        this.url = request.url.slice(urlobj.origin.length);
+
+        this.headersDistinct = {};
+
+        for (const [key, value] of request.headers) {
+            this.headersDistinct[key] = [value];
+        }
+
+        this.headers = {};
+
+        for (const [key, value] of request.headers) {
+            this.headers[key] = value;
+        }
     }
 }
